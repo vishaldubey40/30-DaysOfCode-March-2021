a107efa795901ddc8baac0f8f946ef87eb482040
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-03-20 19:11:02 +0530

    Create Q2.cpp

---
+++ answers/Vishal Dubey/day4/Q2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+   int n;
+   cout<<"Enter the valuse of pattern"<<endl;
+  cin>>>n
+     int number=1;
+  for(int i=1;i<=n;i=i+1)
+    
+  { for(int j=1;j<=i;j++)
+  {cout<<number<<" ";
+   number++;
+  }
+   cout<<endl;
+  }
+  return 0;;
+}
